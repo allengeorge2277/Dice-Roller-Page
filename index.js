7b734eb4cc88d8d739e8173f4358f07c2114a924
allengeorge2277 <150185375+allengeorge2277@users.noreply.github.com>
2025-01-08 23:44:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const rollbtn = document.getElementById("roll-btn");
+
+rollbtn.onclick = function() {
+    let randomNum1 = Math.floor(Math.random()*6 + 1)
+    let randomNum2 = Math.floor(Math.random()*6 + 1)
+
+    const randomDice1 = "dice"+randomNum1+".png";
+    const randomDice2 = "dice"+randomNum2+".png";
+
+    const randomImageSource1 = "/Dice roller/images/"+randomDice1;
+    const randomImageSource2 = "/Dice roller/images/"+randomDice2;
+
+    const image1 = document.querySelectorAll("img")[0];
+    const image2 = document.querySelectorAll("img")[1];
+
+    image1.setAttribute("src",randomImageSource1);
+    image2.setAttribute("src",randomImageSource2);
+
+}
